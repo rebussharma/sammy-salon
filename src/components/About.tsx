cc1254a95010f5a88c6c0de46eb35d127cae6cd6
rebussharma <rebussharma@gmail.com>
2024-03-04 16:45:23 -0600

    about css done

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,11 +4,12 @@ import '../css/About.css'
 
 const About: React.FC = () => {
   const story_1 = "After over 10 years of Sammy pouring her heart and soul into the beauty business, " +
-  "Sammy sensed that a deeper connection with those she served was missing. "+
+  "Sammy sensed that a deeper connection with those she served was missing."
+  const story_2 =
   "Determined to create a space where customers felt truly valued and welcomed, she took a leap of faith and opened this studio"
 
-  const about_sammmy = "Sammy has over 10 years of experience in the beauty industry. " +
-  "Within each stroke of her brush and every gentle conversation, " +
+  const about_sammmy1 = "Sammy has over 10 years of experience in the beauty industry."
+  const about_sammmy2 = "Within each stroke of her brush and every gentle conversation, " +
   "Sammy believes that that success isn't just measured in profits, but in the smiles of those she touched. "
   
   const motto = "Satisfaction over transactions"
@@ -18,30 +19,49 @@ const About: React.FC = () => {
       <div className='title'>
         Story About Us
       </div>
-      <div className='contents'>
-        <div className='mini-circle'></div>
-        <div className='web-title'></div>
-        <div className='story-box'>
-          <div className='story one'>
-            {story_1}
-          </div>
-        </div>
-        
-        <div className='team'>
-          <div className='sammy'>
-            <div className='headshot-name'>
-              < div className='headshot'>
-                <img src={headshot} />
-              </div>
-              <div className='name'>
-                <div className='name-text'>Sammy Shrestha</div>
-              </div>
+      <div className='contents' id='contents'>
+        <div className='content-wrapper'>
+          <div className='mini-circle'></div>
+          <div className='story-box'>
+            <div className='web-title'>
+              <h5>"Satisfaction Before Transaction"</h5>
+              <h2>Our Story</h2>
             </div>
-            <div className='info'>
-              {about_sammmy}
+            <div className='story one'>
+              {story_1}
+              <br></br>
+              <br></br>
+              {story_2}
+            </div>
+            <div className='web-headshot'>
+              <img src={headshot} />
+              <div className='web-name'>
+                  <div className='web-name-text'>Sammy Shrestha</div>
+                </div>
             </div>
           </div>
+          
+          <div className='team'>
+            <div className='sammy'>
+              <div className='headshot-name'>
+                < div className='headshot'>
+                  <img src={headshot} />
+                </div>
+                <div className='name'>
+                  <div className='name-text'>Sammy Shrestha</div>
+                </div>
+              </div>
+              <div className='info'>
+                <h3> Your Esthetician</h3>
+                {about_sammmy1}
+                <br></br>
+                <br></br>
+                {about_sammmy2}
+              </div>
+            </div>
+          </div>
         </div>
+
       </div>
     </div>
   )
