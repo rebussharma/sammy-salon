92cda586ab1f1556e04a6d9b968fb6090eebcf18
rebussharma <rebussharma@gmail.com>
2024-03-06 15:00:23 -0600

    fixed height

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Footer from './components/footer/Footer';
-import Contact from './components/footer/contact/Contact';
 import Home from './components/home/Home';
 import Services from './components/services/Services';
 import Testimonials from './components/testimonials/Testimonials';
@@ -14,7 +13,6 @@ const App: React.FC = () => {
       <Home></Home>  
       <Services></Services>
       <About></About>  
-      <Contact></Contact> 
       <Testimonials></Testimonials>
       <Footer></Footer>
      </div>
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import '../../css/footer/Footer.css'
+import Conact from './contact/Contact'
 
 const Footer:React.FC = () => {
   return (
-    <div className='footer'>All rights reserved to me</div>
+    <div className='footer'>
+      <div className='contact-wrapper'>
+        <Conact></Conact>
+      </div>
+      <div className='footer-details'>
+
+      </div>
+    </div>
   )
 }
 
