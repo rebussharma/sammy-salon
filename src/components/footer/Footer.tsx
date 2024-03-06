b117f0612af51cec795bb523481bc10732cdf8f8
rebussharma <rebussharma@gmail.com>
2024-03-06 15:09:01 -0600

    moved contact to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,11 +5,11 @@ import Conact from './contact/Contact'
 const Footer:React.FC = () => {
   return (
     <div className='footer'>
-      <div className='contact-wrapper'>
-        <Conact></Conact>
-      </div>
       <div className='footer-details'>
 
+      </div>
+      <div className='contact-wrapper'>
+        <Conact></Conact>
       </div>
     </div>
   )
