77e337ad8643b885c5dc6f8b3cc8c678cccefdfb
rebussharma <rebussharma@gmail.com>
2024-06-25 16:07:04 -0500

    fixed api

--- frontend/src/components/appointment/Schedule.tsx
+++ frontend/src/components/appointment/Schedule.tsx
@@ -3,8 +3,9 @@ import { DesktopDateTimePicker } from '@mui/x-date-pickers/DesktopDateTimePicker
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import axios from 'axios';
 import dayjs, { Dayjs } from 'dayjs';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../css/appointment/Schedule.css';
 
 
@@ -33,27 +34,25 @@ type ScheduleSuccess = {
     setAppointmentDateTimeSchedue(dateTime:Dayjs|null):void
 }
 
+
  const Schedule:React.FC<ScheduleSuccess> = ({editData, setScheduleSuccess = () => void{}, setAppointmentDateTimeSchedue =() =>void{}}:ScheduleSuccess) => {
   
-    const bookedDates = [
-    
-        {
-            "appointmentDateTime": "2024-05-10T09:15:00"
-        }
-    
-    ]
-    // const [bookedDates, setBookedDates] = useState([]);
+    const [bookedDates, setBookedDates] = useState([]);
+    const UPCOMING_APPOINTMENT_URL = "http://localhost:8080/api/appointments/confirmed/upcoming"
 
-    // useEffect(() => {  
-    // fetch('http://localhost:8080/api/appointments/relevant')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //     setBookedDates(data);
-    //     })
-    // }, []);
+    useEffect(() => {  
+    axios.get(UPCOMING_APPOINTMENT_URL)
+        .then(response => {
+            console.log(response)
+            setBookedDates(response?.data)
+        })
+        .catch(function (error){
+            console.error("Error Getting Appointments", error.toJSON());
+        })
+    }, []);
+    
     
     const disabledTimes = bookedDates.map((dateTime) => dayjs(dateTime["appointmentDateTime"]));
-
 
     const [selectedDateTime, setSelectedDateTime] = useState <Dayjs|null>(!editData? dayjs(appt_datetime):editData);
 
